d86b19b90b8823da5a0f88ad82bba597c78cc0c0
edmsimplicio <eduardo.msimplicio@gmail.com>
2023-11-02 16:29:11 -0300

    Refactor code and improve node

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -1,19 +1,33 @@
-use crate::node::Node;
+pub struct Node {
+    id: u64,
+    title: String,
+    position: egui::Pos2,
+    working: bool,
+}
 
 pub struct MapReduceApp {
     running: bool,
     num_workers: u8,
     reduce_jobs: u8,
     chunk_size: u32,
+    nodes: Vec<Node>,
 }
 
 impl Default for MapReduceApp {
     fn default() -> Self {
+        let iter = (0..1).map(|_a| Node {
+            id: 0,
+            title: "Node 0".to_string(),
+            position: egui::Pos2::new(250.0, 250.0),
+            working: true,
+        });
+
         Self {
             running: false,
             num_workers: 1,
             reduce_jobs: 2,
             chunk_size: 128,
+            nodes: Vec::from_iter(iter),
         }
     }
 }
@@ -32,43 +46,11 @@ impl MapReduceApp {
 
         Default::default()
     }
-}
 
-impl eframe::App for MapReduceApp {
-    /// Called by the frame work to save state before shutdown.
-    // fn save(&mut self, storage: &mut dyn eframe::Storage) {
-    //     eframe::set_value(storage, eframe::APP_KEY, self);
-    // }
-
-    /// Called each time the UI needs repainting, which may be many times per second.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            egui::menu::bar(ui, |ui| {
-                egui::widgets::global_dark_light_mode_switch(ui);
-
-                ui.separator();
-
-                #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-                {
-                    ui.menu_button("File", |ui: &mut egui::Ui| {
-                        if ui.button("Quit").clicked() {
-                            _frame.close();
-                        }
-                    });
-                }
-            });
-        });
-
+    pub fn render_controls(&mut self, ctx: &egui::Context) {
         egui::SidePanel::right("sidebar").show(ctx, |ui: &mut egui::Ui| {
-            ui.add_space(4.0);
-
-            let node = Node::new(0, "Node 1".to_string(), egui::Pos2::new(250.0, 250.0));
-            node.ui(ui);
-
             ui.heading("Controls");
-
             ui.separator();
-
             ui.horizontal(|ui: &mut egui::Ui| {
                 ui.vertical(|ui: &mut egui::Ui| {
                     ui.add_space(2.5);
@@ -78,7 +60,6 @@ impl eframe::App for MapReduceApp {
                     ui.add_space(13.5);
                     ui.label("Chunk size: ");
                 });
-
                 ui.vertical(|ui: &mut egui::Ui| {
                     ui.add(egui::Slider::new(&mut self.num_workers, 1..=12));
                     ui.add_space(10.0);
@@ -87,13 +68,9 @@ impl eframe::App for MapReduceApp {
                     ui.add(egui::Slider::new(&mut self.chunk_size, 32..=204800))
                 });
             });
-
             ui.add_space(10.0);
-
             ui.separator();
-
             ui.add_space(10.0);
-
             ui.horizontal(|ui: &mut egui::Ui| {
                 let mut button_label = "Run";
                 if self.running {
@@ -106,34 +83,59 @@ impl eframe::App for MapReduceApp {
                 };
             });
         });
+    }
 
-        egui::CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
-            ui.with_layout(
-                egui::Layout::bottom_up(egui::Align::LEFT),
-                |ui: &mut egui::Ui| {
-                    powered_by_egui_and_eframe(ui);
-                    ui.separator();
-                },
-            );
-        });
+    pub fn render_nodes(&mut self, ui: &mut eframe::egui::Ui) {
+        for node in self.nodes.iter_mut() {
+            Self::render_node(node, ui);
+        }
+    }
+
+    fn render_node(node: &mut Node, ui: &mut egui::Ui) {
+        let node_id = ui.make_persistent_id(node.id);
+
+        let _node_window = egui::Area::new(node_id)
+            .default_pos(node.position.clone())
+            .movable(true)
+            .show(ui.ctx(), |ui: &mut egui::Ui| {
+                egui::Frame::none()
+                    .stroke(egui::Stroke::new(2.0, egui::Color32::DARK_GRAY))
+                    .inner_margin(egui::Margin::same(10.0))
+                    .rounding(5.0)
+                    .show(ui, |ui| {
+                        ui.vertical(|ui| {
+                            ui.horizontal(|ui| {
+                                let mut color = egui::Color32::DARK_GREEN;
+                                if node.working == false {
+                                    color = egui::Color32::DARK_RED;
+                                }
+                                ui.label(node.title.clone());
+                                egui::Frame::none()
+                                    .fill(color)
+                                    .rounding(9999.0)
+                                    .inner_margin(egui::Margin::same(5.0))
+                                    .show(ui, |_ui| {});
+                            });
+                            let button_label = if node.working {
+                                "Force worker failure"
+                            } else {
+                                "Fix worker"
+                            };
+                            let button: egui::Button<'_> =
+                                egui::Button::new(button_label).min_size(egui::vec2(50.0, 30.0));
+                            if ui.add(button).clicked() {
+                                node.working = !(node.working.clone());
+                                ui.ctx().request_repaint()
+                            };
+                        });
+                    });
+            });
     }
 }
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui: &mut egui::Ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("See source code: ");
-        ui.hyperlink_to(
-            "distributed-rust",
-            "https://github.com/Costa-SM/distributed-rust",
-        );
-        ui.label(". Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
-}
+// impl App for MapReduceApp {
+//     // Called by the frame work to save state before shutdown.
+//     // fn save(&mut self, storage: &mut dyn eframe::Storage) {
+//     //     eframe::set_value(storage, eframe::APP_KEY, self);
+//     // }
+// }
--- src/gui/main.rs
+++ src/gui/main.rs
@@ -1,16 +1,66 @@
 mod app;
-mod node;
-use crate::app::MapReduceApp;
+
+use app::MapReduceApp;
+
+impl eframe::App for MapReduceApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        render_header(ctx, frame);
+        self.render_controls(ctx);
+        egui::CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
+            self.render_nodes(ui);
+            render_footer(ui);
+        });
+    }
+}
+
+fn render_header(ctx: &egui::Context, frame: &mut eframe::Frame) {
+    egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+        egui::menu::bar(ui, |ui| {
+            egui::widgets::global_dark_light_mode_switch(ui);
+            ui.add_space(4.0);
+            ui.separator();
+            {
+                ui.menu_button("File", |ui: &mut egui::Ui| {
+                    if ui.button("Quit").clicked() {
+                        frame.close();
+                    }
+                });
+            }
+        });
+    });
+}
+
+fn render_footer(ui: &mut egui::Ui) {
+    ui.with_layout(
+        egui::Layout::bottom_up(egui::Align::LEFT),
+        |ui: &mut egui::Ui| {
+            ui.horizontal(|ui: &mut egui::Ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.label("See source code: ");
+                ui.hyperlink_to(
+                    "distributed-rust",
+                    "https://github.com/Costa-SM/distributed-rust",
+                );
+                ui.label(". Powered by ");
+                ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+                ui.label(" and ");
+                ui.hyperlink_to(
+                    "eframe",
+                    "https://github.com/emilk/egui/tree/master/crates/eframe",
+                );
+                ui.label(".");
+            });
+            ui.separator();
+        },
+    );
+}
 
 fn main() -> eframe::Result<()> {
-    let native_options: eframe::NativeOptions = eframe::NativeOptions {
-        initial_window_size: Some([1440.0, 760.0].into()),
-        min_window_size: Some([1440.0, 760.0].into()),
-        ..Default::default()
-    };
+    let mut window_options: eframe::NativeOptions = eframe::NativeOptions::default();
+    window_options.initial_window_size = Some(egui::Vec2::new(1440.0, 760.0));
     eframe::run_native(
         "MapReduce GUI",
-        native_options,
+        window_options,
         Box::new(|cc: &eframe::CreationContext<'_>| Box::new(MapReduceApp::new(cc))),
     )
 }
