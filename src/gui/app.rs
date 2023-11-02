48026079567dae6730e22f3e43a8778d0e5dc3e0
edmsimplicio <eduardo.msimplicio@gmail.com>
2023-11-02 16:57:47 -0300

    Workers sliders working

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -1,5 +1,7 @@
-pub struct Node {
-    id: u64,
+use egui::emath::Numeric;
+
+pub struct Worker {
+    id: u8,
     title: String,
     position: egui::Pos2,
     working: bool,
@@ -10,41 +12,19 @@ pub struct MapReduceApp {
     num_workers: u8,
     reduce_jobs: u8,
     chunk_size: u32,
-    nodes: Vec<Node>,
-}
-
-impl Default for MapReduceApp {
-    fn default() -> Self {
-        let iter = (0..1).map(|_a| Node {
-            id: 0,
-            title: "Node 0".to_string(),
-            position: egui::Pos2::new(250.0, 250.0),
-            working: true,
-        });
-
-        Self {
-            running: false,
-            num_workers: 1,
-            reduce_jobs: 2,
-            chunk_size: 128,
-            nodes: Vec::from_iter(iter),
-        }
-    }
+    nodes: Vec<Worker>,
 }
 
 impl MapReduceApp {
     /// Called once before the first frame.
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        // if let Some(storage) = cc.storage {
-        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        // }
-
-        Default::default()
+        Self {
+            running: false,
+            num_workers: 0,
+            reduce_jobs: 2,
+            chunk_size: 128,
+            nodes: Vec::new(),
+        }
     }
 
     pub fn render_controls(&mut self, ctx: &egui::Context) {
@@ -61,7 +41,22 @@ impl MapReduceApp {
                     ui.label("Chunk size: ");
                 });
                 ui.vertical(|ui: &mut egui::Ui| {
-                    ui.add(egui::Slider::new(&mut self.num_workers, 1..=12));
+                    if ui
+                        .add(egui::Slider::new(&mut self.num_workers, 0..=8))
+                        .changed()
+                    {
+                        self.nodes.clear();
+                        for i in 0..self.num_workers {
+                            let node_x_pos: f64 = 50.0 + i.to_f64() * 135.0;
+                            self.nodes.push(Worker {
+                                id: i + 1,
+                                title: format!("Worker {}", i + 1),
+                                position: egui::Pos2::new(node_x_pos as f32, 50.0),
+                                working: true,
+                            });
+                        }
+                    };
+
                     ui.add_space(10.0);
                     ui.add(egui::Slider::new(&mut self.reduce_jobs, 2..=32));
                     ui.add_space(10.0);
@@ -91,12 +86,12 @@ impl MapReduceApp {
         }
     }
 
-    fn render_node(node: &mut Node, ui: &mut egui::Ui) {
+    fn render_node(node: &mut Worker, ui: &mut egui::Ui) {
         let node_id = ui.make_persistent_id(node.id);
 
         let _node_window = egui::Area::new(node_id)
             .default_pos(node.position.clone())
-            .movable(true)
+            .movable(false)
             .show(ui.ctx(), |ui: &mut egui::Ui| {
                 egui::Frame::none()
                     .stroke(egui::Stroke::new(2.0, egui::Color32::DARK_GRAY))
@@ -110,6 +105,7 @@ impl MapReduceApp {
                                     color = egui::Color32::DARK_RED;
                                 }
                                 ui.label(node.title.clone());
+                                ui.add_space(20.0);
                                 egui::Frame::none()
                                     .fill(color)
                                     .rounding(9999.0)
@@ -117,12 +113,12 @@ impl MapReduceApp {
                                     .show(ui, |_ui| {});
                             });
                             let button_label = if node.working {
-                                "Force worker failure"
+                                "Break worker"
                             } else {
                                 "Fix worker"
                             };
                             let button: egui::Button<'_> =
-                                egui::Button::new(button_label).min_size(egui::vec2(50.0, 30.0));
+                                egui::Button::new(button_label).min_size(egui::vec2(90.0, 30.0));
                             if ui.add(button).clicked() {
                                 node.working = !(node.working.clone());
                                 ui.ctx().request_repaint()
