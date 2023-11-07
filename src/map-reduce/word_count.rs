a2bfbcae23155c4efdc39ec66574e2a034d29945
Arthur Stevenson <arthurcstevenson@gmail.com>
2023-11-07 15:45:20 -0300

    Added a map/reduce routine for counting words

---
+++ src/map-reduce/word_count.rs
@@ -0,0 +1,71 @@
+#[derive(Clone)]
+pub struct KeyValue {
+    pub key: String,
+    pub value: i32
+}
+
+// This function receives a string obtained from the split files.
+pub fn map_func(buffer: &[u8]) -> Vec<KeyValue> {
+    // This considers UTF-8 encoding.
+    let words = match std::str::from_utf8(buffer) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    // Make all characters lowercase, and remove the punctuation. Then, separate words by whitespace.
+    let words = words.to_ascii_lowercase().replace(&['(', ')', ',', '\"', '.', ';', ':', '\''][..], "");
+    let split = words.split_ascii_whitespace();
+    
+    // Create a vector to store the words.
+    let mut result: Vec<KeyValue> = Vec::new();
+
+    for word in split {
+        result.push(KeyValue{key: word.to_string(), value: 1})
+    }
+
+    return result
+}
+
+pub fn reduce_func(inputs: &mut Vec<KeyValue>) -> &mut Vec<KeyValue> {
+    // Find repeated words and their indices, to remove them from the key/value pairs.
+    // Use the KeyValue struct as an indicator of the word and the position.
+    let mut removed_words: Vec<KeyValue> = Vec::new();
+
+    for (iter, item) in inputs.iter().enumerate() {
+        let indices = inputs
+                                        .iter()
+                                        .enumerate()
+                                        .filter(|(i, k)| k.key == item.key && i > &iter)
+                                        .map(|(index, _)| index)
+                                        .collect::<Vec<_>>();
+        
+        for index in indices {
+            removed_words.push(KeyValue {key: item.key.clone(), value: index as i32});
+        }
+    }
+
+    // Sort the indices so that when they are removed from the vector, they are not shifted around.
+    removed_words.sort_unstable_by(|a, b| b.value.cmp(&a.value));
+
+    // Remove repeated words from the vector.
+    for word in removed_words {
+        let index = inputs.iter().position(|w| w.key == word.key).unwrap();
+
+        inputs[index].value += 1;
+        inputs.remove(word.value as usize);
+    }
+
+    return inputs;
+}
+
+/* Main function for testing the Word Count algorithm. */
+fn main() {
+    let test_string: &str = "hello, this is a TeSt tEst. stRIng; with soME: repeated. words words";
+    let mut dict = map_func(test_string.as_bytes());
+
+    let reduced_dict = reduce_func(&mut dict);
+    
+    for item in reduced_dict.clone() {
+        println!("Word/Value: {} / {}", item.key, item.value);
+    }
+}
