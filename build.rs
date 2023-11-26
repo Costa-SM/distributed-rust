6febeb8dd3ed37e26200d4c4d76574a78d6908ba
Arthur Stevenson <arthurcstevenson@gmail.com>
2023-11-26 14:59:28 -0300

    Revert "Partial implementation of RPCs. Moving from one PC to the other"
    This reverts commit 7cd0e47d385b30bec62a1cae12daf69edd09c3db.

--- build.rs
+++ build.rs
@@ -1,8 +1,7 @@
 fn main () -> Result<(), Box<dyn std::error::Error>> {
     // Voting example server/client
     tonic_build::compile_protos("src/voting/voting.proto")?;
-
-    // MapReduce RPC
-    tonic_build::compile_protos("src/mapreduce/mapreduce.proto")?;
     Ok(())
+
+    // Voting example client
 }
